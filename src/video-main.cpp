c37ba1e64cdc1c86be256c1d100edc01b090db46
Green Sky <green@g-s.xyz>
2023-04-25 22:56:03 +0200

    spaces to tabs

--- src/video-main.cpp
+++ src/video-main.cpp
@@ -17,27 +17,27 @@ struct MumbleAPI_v_1_2_x mumbleAPI;
 mumble_plugin_id_t ownID;
 
 SDL_Window* init_sdl(const char* window_title, int window_pos_x, int window_pos_y, int window_width, int window_height, uint32_t flags){
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
-        return NULL;
-    }
+	if(SDL_Init(SDL_INIT_VIDEO) < 0){
+		return NULL;
+	}
 
-    main_window = SDL_CreateWindow(window_title, window_pos_x, window_pos_y, window_width, window_height, flags);
+	main_window = SDL_CreateWindow(window_title, window_pos_x, window_pos_y, window_width, window_height, flags);
 
-    if(main_window == NULL){
-        return NULL;
-    }
+	if(main_window == NULL){
+		return NULL;
+	}
 
-    main_surface = SDL_GetWindowSurface(main_window);
+	main_surface = SDL_GetWindowSurface(main_window);
 
-    if(main_surface == NULL){
-        return NULL;
-    }
+	if(main_surface == NULL){
+		return NULL;
+	}
 
-    SDL_FillRect(main_surface, NULL, SDL_MapRGB(main_surface->format, 0xFF, 0xAA, 0xFF));
+	SDL_FillRect(main_surface, NULL, SDL_MapRGB(main_surface->format, 0xFF, 0xAA, 0xFF));
 
-    SDL_UpdateWindowSurface(main_window);
+	SDL_UpdateWindowSurface(main_window);
 
-    return main_window;
+	return main_window;
 }
 
 void mumble_shutdown() {
@@ -45,7 +45,7 @@ void mumble_shutdown() {
 }
 
 MumbleStringWrapper mumble_getName() {
-    static const std::string_view name {"video"};
+	static const std::string_view name {"video"};
 
 	return {
 		name.data(),
@@ -55,7 +55,7 @@ MumbleStringWrapper mumble_getName() {
 }
 
 MumbleStringWrapper mumble_getAuthor() {
-    static const std::string_view author {"Erik Scholz & David Zero"};
+	static const std::string_view author {"Erik Scholz & David Zero"};
 
 	return {
 		author.data(),
@@ -85,7 +85,7 @@ mumble_version_t mumble_getVersion() {
 }
 
 mumble_version_t mumble_getAPIVersion() {
-    return MUMBLE_PLUGIN_API_VERSION;
+	return MUMBLE_PLUGIN_API_VERSION;
 }
 
 void mumble_registerAPIFunctions(void* apiStruct) {
@@ -96,43 +96,43 @@ void mumble_releaseResource(const void *) {
 }
 
 int video_main(void* data) {
-    if(!init_sdl("Muh Video Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 400, SDL_WINDOW_BORDERLESS)){
-        return -1;
-    }
+	if(!init_sdl("Muh Video Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 400, SDL_WINDOW_BORDERLESS)){
+		return -1;
+	}
 
-    SDL_Event e;
+	SDL_Event e;
 
-    while (!SDL_AtomicGet(&should_quit)) {
-        //Blit frame
-        auto surface = q.pop();
+	while (!SDL_AtomicGet(&should_quit)) {
+		//Blit frame
+		auto surface = q.pop();
 
-        if(surface.has_value()){
-            SDL_BlitSurface(*surface, NULL, main_surface, NULL);
+		if(surface.has_value()){
+			SDL_BlitSurface(*surface, NULL, main_surface, NULL);
 
-            SDL_FreeSurface(*surface);
+			SDL_FreeSurface(*surface);
 
-            SDL_UpdateWindowSurface(main_window);
-        }
+			SDL_UpdateWindowSurface(main_window);
+		}
 
-        while(SDL_PollEvent(&e) != 0) {
-            switch(e.type){
-                case SDL_QUIT:
+		while(SDL_PollEvent(&e) != 0) {
+			switch(e.type){
+				case SDL_QUIT:
 					// TODO: destroy instead lol
-                    SDL_HideWindow(main_window);
+					SDL_HideWindow(main_window);
 
-                    break;
-                case SDL_WINDOWEVENT:
-                    switch(e.window.event){
-                        case SDL_WINDOWEVENT_CLOSE:
-                            SDL_HideWindow(main_window);
-                    }
+					break;
+				case SDL_WINDOWEVENT:
+					switch(e.window.event){
+						case SDL_WINDOWEVENT_CLOSE:
+							SDL_HideWindow(main_window);
+					}
 
-                    break;
-            }
-        }
-    }
+					break;
+			}
+		}
+	}
 
-    SDL_Quit();
+	SDL_Quit();
 
 	return 0;
 }
@@ -142,14 +142,14 @@ mumble_error_t mumble_init(mumble_plugin_id_t pluginID) {
 
 	SDL_AtomicSet(&should_quit, 0);
 
-    SDL_Thread* t = SDL_CreateThread(video_main, "video main thread", NULL);
+	SDL_Thread* t = SDL_CreateThread(video_main, "video main thread", NULL);
 
-    if (!t) {
-        return MUMBLE_EC_GENERIC_ERROR;
-    }
+	if (!t) {
+		return MUMBLE_EC_GENERIC_ERROR;
+	}
 
-    SDL_DetachThread(t);
+	SDL_DetachThread(t);
 
-    return MUMBLE_STATUS_OK;
+	return MUMBLE_STATUS_OK;
 }
 
